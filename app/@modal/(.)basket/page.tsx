bc782e43ffbb49f256d277d834a7e565de27df5d
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-02-14 23:20:59 +0530

    all completed

--- app/@modal/(.)basket/page.tsx
+++ app/@modal/(.)basket/page.tsx
@@ -30,11 +30,12 @@ function BasketInterseption() {
                 }
             }}
         >
-            <DialogContent className="h-4/5 w-full overflow-scroll max-w-3xl">
+            <DialogContent className="h-4/5 w-full overflow-x-hidden overflow-scroll max-w-3xl">
                 <DialogHeader>
                     <DialogTitle>Basket</DialogTitle>
-                    <DialogDescription>
+                    <DialogDescription >
                         <p className="text-2xl">Contents of your Basket</p>
+
                         <p className="text-xl sm:text-xs font-light text-black">
                             {cart.length > 0 ? `${cart.length} items` : "My Cart"}
                         </p>
